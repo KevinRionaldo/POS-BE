e2fa4da8e9d2e2acca3b0a64e1edc6c455c0f420
Kevin Rionaldo <kevinhutabarat4@gmail.com>
2025-02-22 18:02:57 +0700

    add payments

--- services/payments.go
+++ services/payments.go
@@ -17,8 +17,9 @@ import (
 func CreatePayment(c *gin.Context) {
 	//parse body string to object
 	body := midtransService.MidtransNotification{}
+	log.Info().Any("event", c).Msg("log event")
 	if err := c.BindJSON(&body); err != nil {
-		return
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": apiResponse.GeneralErrorResponse(err)})
 	}
 
 	//convert amout from string to float64
