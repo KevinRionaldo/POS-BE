cc7027bf69c3a06757f8200941ca13bea2ec5bae
Kevin Rionaldo <kevinhutabarat4@gmail.com>
2025-02-22 18:33:27 +0700

    delete localhost after get transactions by id

--- main.go
+++ main.go
@@ -29,6 +29,6 @@ func main() {
 	//payments RESTAPI
 	r.POST("/payments", services.CreatePayment)
 
-	r.Run("localhost:4000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	// r.Run(":4000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// r.Run("localhost:4000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":4000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
