40fa27ee5b804ec6abc5ee1507012e05dd6f7a81
Kevin Rionaldo <kevinhutabarat4@gmail.com>
2025-05-20 16:17:36 +0700

    Stop tracking .env file

--- main.go
+++ main.go
@@ -36,5 +36,5 @@ func main() {
 	//payments RESTAPI
 	r.POST("/payments", services.CreatePayment)
 
-	r.Run(":4000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// r.Run(":4000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
